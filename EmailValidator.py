8ecb68ea5c2be2f8d37f8c32bdf426e5420cf477
araj15006 <39117090+araj15006@users.noreply.github.com>
2023-12-09 04:40:45 +0530

    Update EmailValidator.py

--- EmailValidator.py
+++ EmailValidator.py
@@ -11,17 +11,23 @@ def emailTest(email):
         and ((" .in" in email) or (".com" in email))
     ):
         for item in email:
-            if item.isupper():
-                valid = 1
-                break
-            elif not item.isalnum():
+            if not item.isalnum():
                 special += 1
                 if special > 2:
                     valid = 1
                     break
             else:
                 valid = 0
-        print("Valid email" if valid == 0 else "Invalid email")
+        if valid == 1 :
+            print("invalid")
+        else:
+            id,domain = email.split('@')
+            for i in domain:
+                if i.isupper():
+                    valid = 1
+                else :
+                    valid = 0 
+        print("valid email" if valid ==0 else "invalid email")
     else:
         print("Invalid email")
 
